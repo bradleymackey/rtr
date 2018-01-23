7b50cc0ff55aeb23ccbadae6b14b515bb8e5b2bd
Bradley Mackey <bradleymackey98@googlemail.com>
2018-01-23 12:25:22 +0000

    added notifications for new news and events

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -7,7 +7,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.notifyVolunteer = functions.database.ref('volunteers/{someID}').onCreate(event => {
+exports.notifyVolunteer = functions.database.ref('volunteers/{newVolunteer}').onCreate(event => {
     const event_snapshot = event.data;
     const first_name = event_snapshot.first_name.val();
     const last_name = event_snapshot.last_name.val();
@@ -18,5 +18,39 @@ exports.notifyVolunteer = functions.database.ref('volunteers/{someID}').onCreate
           body: `${first_name} ${last_name} volunteered for ${for_event}`,
         }
       };
-    return admin.messaging().sendToTopic('admin', payload);
-})
+    return admin.messaging().sendToTopic('admin', payload).catch(error => {
+        console.log(`error sending volunteer notification: ${error}`);
+    });
+});
+
+exports.notifyEvent = functions.database.ref('events/{newEvent}').onCreate(event => {
+    const event_snapshot = event.data;
+    const event_title = event_snapshot.title;
+    const payload = {
+        notification: {
+            title: 'New Event',
+            body: `"${event_title}" has just been posted!`
+        }
+    };
+    // event will only live for 1 week
+    const options = {
+        timeToLive: 60 * 60 * 24 * 7
+    };
+    return admin.messaging().sendToTopic('events', payload, options).catch(error => {
+        console.log(`error sending events notification: ${error}`);
+    });
+});
+
+exports.notifyNews = functions.database.ref('news/{newArticle}').onCreate(event => {
+    const event_snapshot = event.data;
+    const article_title = event_snapshot.title;
+    const payload = {
+        notification: {
+            title: 'New Article',
+            body: `${article_title}`
+        }
+    };
+    return admin.messaging().sendToTopic('news', payload).catch(error => {
+        console.log(`error sending news notification: ${error}`);
+    });
+});
