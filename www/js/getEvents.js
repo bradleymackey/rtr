bcab362c378b554850ec5b82b9c4237d492382b3
Nia Gardner <niajanegardner@gmail.com>
2018-02-04 09:55:32 +0000

    link event detail to database

--- www/js/getEvents.js
+++ www/js/getEvents.js
@@ -17,24 +17,26 @@ firebase.database().ref("/events").orderByChild("title").once('value').then(func
       $('#events_main').empty();
       $('#events_main').html(f);
       var r = '';
-      $("#events .content-item").click(displayEventContentItem);
+      $("#events.content-item").click(displayEventContentItem);
       $('h2').click(function(){
         var eid = $(this).attr("id");
-        console.log(eid);
-        data = 'event.'+eid;
-        console.log(data);
-        console.log(event.birdsEvent);
-        r+= '<img src='+data.image+' alt="image" style="width:100%;"><div style="padding: 15px;">'
-        r+= '<h1 style="color: #2dccd3; text-align: left;">'+data.title+'</h1>'
-        r+=  '<p class="detail">'+data.content+'</p> <h2>Event Details</h2>'
-        r+= '<p><img class="icon" src="img/where-icon.png"><b> WHERE</b></br>'+data.location_1+'</br>'+data.location_2+'</p>'
+        ev = event[eid];
+        console.log(event['birdsEvent']);
+        r+= '<img src='+ev.image+' alt="image" style="width:100%;"><div style="padding: 15px;">'
+        r+= '<h1 style="color: #2dccd3; text-align: left;">'+ev.title+'</h1>'
+        r+=  '<p class="detail">'+ev.content+'</p> <h2>Event Details</h2>'
+        r+= '<p><img class="icon" src="img/where-icon.png"><b> WHERE</b></br>'+ev.location_1+'</br>'+ev.location_2+'</p>'
         r+= '<button type="button" class="event_b">VIEW ON MAP</button> <hr>'
         r+=  '<p><img class="icon" src="img/when-icon.png"><b> WHEN</b></br>DATE</br> End: 31st January, 2018 at 3:00pm</p>'
         r+=  '<button type="button" class="event_b">ADD TO CALENDAR</button></div>'
+        console.log(r);
+        $('#events_main').hide();
+        $('#event_detail').show();
+        $('#event_detail').empty();
+        $('#event_detail').html(r);
       //  $('div#'+eid).toggle();
     });
-    $('#event_detail').empty();
-    $('#event_detail').html(r);
+
 
   });
 
