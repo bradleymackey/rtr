4f531902bd9d27b8b088b333bf4036bb0de6bf93
Bradley Mackey <bradleymackey98@googlemail.com>
2018-02-06 09:57:08 +0000

    location working :D

--- platforms/ios/RTR.xcarchive/Products/Applications/RTR.app/www/js/map.js
+++ platforms/ios/RTR.xcarchive/Products/Applications/RTR.app/www/js/map.js
@@ -4,16 +4,21 @@
 
  "use strict";
 
-  document.addEventListener("deviceready", function(event) {
+ var map;
+
+ document.addEventListener("DOMContentLoaded", function(event) {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYnJhZGxleW1hY2tleSIsImEiOiJjamNraW14eXYzanZzMzNwZ2UyNW9ua2tzIn0.4YBdPgKnDN5XFdVVMeo4LQ';
-    navigator.geolocation.getCurrentPosition(function(position) {
-        alert(position.coords);
-    });
-    var map = new mapboxgl.Map({
+    map = new mapboxgl.Map({
         container: 'map-layer',
         style: 'mapbox://styles/bradleymackey/cjckjuujz01qh2spiu3lh87uu'
     });
-    // Add geolocate control to the map.
+ });
+
+  document.addEventListener("deviceready", function(event) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        // alert(position.coords);
+     });
+      // Add geolocate control to the map.
     map.addControl(new mapboxgl.GeolocateControl({
         positionOptions: {
             enableHighAccuracy: true
@@ -42,15 +47,18 @@
   }, false);
 // do not adjust values
   $(window).on("resize", function () { 
+    updateMapSize();
+}).trigger("resize");
+
+function updateMapSize() {
     var div = document.createElement('div');
     div.style.paddingTop = 'env(safe-area-inset-top)';
     div.style.paddingBottom = 'env(safe-area-inset-bottom)';
     document.body.appendChild(div);
     const calculatedPadding =  parseInt(window.getComputedStyle(div).paddingTop) + parseInt(window.getComputedStyle(div).paddingBottom);
     $("#map-layer").height($(window).height()-$(".topnav").height()-$(".tabbar").height()-calculatedPadding);
-    //map.invalidateSize(); 
+    map.invalidateSize(); 
     document.body.removeChild(div);
-}).trigger("resize");
-
+}
 
  
--- platforms/ios/RTR.xcarchive/Products/Applications/RTR.app/www/js/navigation.js
+++ platforms/ios/RTR.xcarchive/Products/Applications/RTR.app/www/js/navigation.js
@@ -4,6 +4,9 @@
 
 "use strict";
 
+//stores current page for back button.
+var currentPage = 0;
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -13,20 +16,45 @@ function onDeviceReady() {
     });
 }
 
-$("#map-icon").click(function() {
+$("#map-icon").click(displayMap);
+
+$("#news-icon").click(displayNews);
+
+$("#events-icon").click(displayEvents);
+
+$("#info-icon").click(displayInfo);
+
+$("#infoToVisions").click(displayVisions);
+
+$("#infoToVolunteer").click(displayVolunteer);
+
+$("#infoToLeaflets").click(displayLeaflets);
+
+//show event detail
+$("#events .content-item").click(displayEventContentItem);
+
+$("#news .content-item").click(displayNewsContentItem);
+
+$("#volunteer-ops .content-item").click(displayVolunteerSignup);
+
+function displayMap(){
     // hide content, because the map requires a different layout
     $(".content").css("display", "none");
     tabItemSelected($("#map-icon"),$("#map"));
     updateTitle("Map");
+    $("#backbutton").hide();
+    updateMapSize();
+    currentPage = 0;
     return false;
-});
+}
 
-$("#news-icon").click(function() {
+function displayNews(){
     // display content again (because map hides it)
     $(".content").css("display", "block");
     $(".content").css({"margin-top": "3.0 em"});
     tabItemSelected($("#news-icon"),$("#news"));
     updateTitle("");
+    $("#backbutton").hide();
     $("#topnav-title").append(`
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="Search News">
@@ -34,17 +62,19 @@ $("#news-icon").click(function() {
     );
     $(".content-item").show();
     $("#news-article").hide();
+    currentPage = 3;
     return false;
-});
+}
 
-$("#events-icon").click(function() {
+function displayEvents(){
     // display content again (because map hides it)
     $(".content").css("display", "block");
     $(".content").css({"margin-top": "3.0 em"});
     $(".event-section").hide();
-    $(".events_main").show();
+    $("#events_main").show();
     tabItemSelected($("#events-icon"),$("#events"));
     updateTitle("");
+    $("#backbutton").hide();
     $("#topnav-title").append(`
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="Search Events">
@@ -52,10 +82,11 @@ $("#events-icon").click(function() {
     );
     $(".content-item").show();
     $("#event_detail").hide();
+    currentPage = 5;
     return false;
-});
+}
 
-$("#info-icon").click(function() {
+function displayInfo(){
     // display content again (because map hides it)
     $(".content").css("display", "block");
     //changes margin so that there isnt a space between navbar and logo
@@ -64,53 +95,103 @@ $("#info-icon").click(function() {
     $(".info_main").show();
     tabItemSelected($("#info-icon"),$("#info"));
     updateTitle("Information");
+    $("#backbutton").hide();
+    currentPage = 7;
     return false;
-});
+}
 
-$("#infoToVisions").click(function() {
+function displayVisions(){
     //changes margin so that there isnt a space between navbar and logo
     $(".container").hide();
     $("#visions-aims").show();
     updateTitle("Visions and Aims");
+    $("#backbutton").show();
+    currentPage = 8;
     return false;
-});
+}
 
-$("#infoToVolunteer").click(function() {
+function displayVolunteer(){
     //changes margin so that there isnt a space between navbar and logo
     $(".info_main").hide();
     $("#volunteer-ops").show();
     $(".info_images").hide();
     updateTitle("Volunteer Opportunities");
+    $("#backbutton").show();
+    currentPage = 9;
     return false;
-});
+}
 
-$("#infoToLeaflets").click(function() {
+function displayVolunteerSignup(){
+    $("#volunteer-ops").hide();
+    $("#volunteer-signup").show();
+    updateTitle("Volunteer sign up");
+    $("#backbutton").show();
+    currentPage = 10;
+    return false;
+}
+
+function displayLeaflets(){
     //changes margin so that there isnt a space between navbar and logo
     $(".info_main").hide();
     $("#leaflets").show();
     $(".info_images").hide();
     updateTitle("Leaflets");
+    $("#backbutton").show();
+    currentPage = 10;
     return false;
-});
+}
 
-//show event detail
-$("#events .content-item").click(function() {
+function displayEventContentItem(){
     //changes margin so that there isnt a space between navbar and logo
     $(".events_main").hide();
+    $("#backbutton").show();
     $("#event_detail").show();
     updateTitle("Event Detail");
+    currentPage = 6;
     return false;
-});
+}
 
-$("#news .content-item").click(function(){
+function displayNewsContentItem(){
+    $("#backbutton").show();
     $(".content-item").hide();
     $("#news-article").show();
     updateTitle("Some Article");
+    currentPage = 4;
+    return false;
+}
+
+function backButtonPressed(){
+    switch(currentPage){
+        case 1:
+        case 2:
+            displayMap();
+            break;
+        case 4:
+            displayNews();
+            break;
+        case 6:
+            displayEvents();
+            break;
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+            displayInfo();
+            break;
+        case 12:
+            displayLeaflets();
+    }
+}
+
+document.addEventListener("backbutton", function(){
+   backButtonPressed();
 });
 
 // update the top title of the navigation bar when a new section is selected
 function updateTitle(titleText) {
     $("#topnav-title").text(titleText);
+    // if we are changing the title, we are likley changing page, so scroll to top
+    window.scrollTo(0, 0);
 }
 
 // when a tab item is selected, hide all current tab items and then only show the currently selected one
